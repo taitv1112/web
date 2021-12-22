c7efceaa077fe223971ca7aa33340c03074d1a3e
taitv1112 <91813628+taitv1112@users.noreply.github.com>
2021-12-22 10:50:00 +0700

    tao web

---
+++ src/main/java/Home.java
@@ -0,0 +1,33 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet("/home")
+public class Home extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("goi home");
+        resp.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = resp.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+        Date today = new Date();
+
+        out.println("<h1>" + today + "</h1>");
+        out.println("</html>");
+    }
+}
